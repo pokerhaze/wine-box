7ca8b8442116d38e354a69970b9d53159768385a
Ubuntu <ubuntu@sg-arm-5.tail43ff5.ts.net>
2024-10-04 19:03:11 +0000

    removed useless files.

--- root/init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-for i in `ls /init/*|sort`
-do
-[ -x "$i" ] && {
-    "$i"
-}
-done
--- root/init/002-install-wine-python-pip.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# download
-wget https://bootstrap.pypa.io/get-pip.py
-wine python get-pip.py
